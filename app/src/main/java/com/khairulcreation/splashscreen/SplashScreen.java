472ae085b38e4587ea08c536c670fb37b125577f
khairulhelmie <30465528+khairulhelmie@users.noreply.github.com>
2018-02-28 23:05:54 +0800

    first commit

---
+++ app/src/main/java/com/khairulcreation/splashscreen/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.khairulcreation.splashscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    //3000 tu 3 seconds
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+            myThread.start();
+    }
+}
